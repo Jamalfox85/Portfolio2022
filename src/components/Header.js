509c9aafcd171eb3526b641ae2dc914336041206
Jamal Fox <jamalfox@Jamals-MacBook-Air.local>
2022-07-17 14:42:35 -0400

    Completed html and basic styling for navbar and header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div class="nav-wrapper">
+			<div class="expand-menu">
+				<div class="expand-menu-line" id="line-1"></div>
+				<div class="expand-menu-line" id="line-2"></div>
+				<div class="expand-menu-line" id="line-3"></div>
+			</div>
+			<div>
+				<button class="contact-button">Contact</button>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div class="hero-wrapper">
+			<h1>Jamal Fox</h1>
+			<h2>Front End Developer</h2>
+			<button class="contact-button">Contact</button>
+		</div>
+	);
+};
+
+export default Hero;
